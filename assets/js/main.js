5aa439a0e5fc0414c47b325f218d45e8503f764a
manoj sharma <mh19992099@gmail.com>
2024-01-25 02:30:07 +0530

    1st small project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,24 @@
-tl = gsap.timeline();
-tl.to("h1",{
-    x:600,
-    duration:1
+var tl = gsap.timeline();
+tl.from("nav img, nav a", {
+    y:-100,
+    opacity:0,
+    duration:1,
+    stagger:0.1
 })
-tl.to("h2",{
-    x:700,
-    duration:1
+tl.from("h1",{
+    y:100,
+    opacity:0,
+    stagger:0.2
 })
-tl.to("h3",{
-    x:800,
-    duration:1
+tl.from("#main img",{
+    scale:0.5,
+    opacity:0,
+    stagger:0.2
+})
+tl.from("#main h5",{
+    y:10,
+    // opacity:0,
+    duration:.5,
+    repeat:-1,
+    yoyo:true
 })
